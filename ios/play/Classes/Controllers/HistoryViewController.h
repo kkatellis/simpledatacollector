3661d5524a7b3fb82e31e2a90f95a98dc1ae79d7
Andrew Huynh <a5.t.huynh@gmail.com>
2011-11-28 19:55:22 -0800

    Removed references to old project name

--- ios/play/Classes/Controllers/HistoryViewController.h
+++ ios/play/Classes/Controllers/HistoryViewController.h
@@ -1,6 +1,6 @@
 //
 //  HistoryViewController.h
-//  play
+//  rockmyworld
 //
 //  Created by Andrew Huynh on 11/14/11.
 //  Copyright (c) 2011 athlabs. All rights reserved.
--- ios/play/Classes/Controllers/MusicViewController.h
+++ ios/play/Classes/Controllers/MusicViewController.h
@@ -1,6 +1,6 @@
 //
 //  MusicViewController.h
-//  play
+//  rockmyworld
 //
 //  Created by Andrew Huynh on 11/14/11.
 //  Copyright (c) 2011 athlabs. All rights reserved.
