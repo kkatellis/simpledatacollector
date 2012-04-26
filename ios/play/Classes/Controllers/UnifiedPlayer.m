d57efbbdce5f8af52a6d1a61dadbc8c6512fcd24
Andrew Huynh <a5.t.huynh@gmail.com>
2012-04-26 11:49:39 -0700

    Added another null check in UnifiedPlayer

--- ios/play/Classes/Controllers/UnifiedPlayer.m
+++ ios/play/Classes/Controllers/UnifiedPlayer.m
@@ -183,7 +183,8 @@ static Rdio *rdio = NULL;
         isPlayingLocal = YES;
     
     //--// Otherwise attempt to stream from RDIO/another location.
-    } else if( [currentTrack isRdio] && [currentTrack rdioId] != nil && [[currentTrack rdioId] length] > 0 ) {
+    } else if( [currentTrack isRdio] && [currentTrack rdioId] != nil && 
+              ![[currentTrack rdioId] isEqual: [NSNull null]] && [[currentTrack rdioId] length] > 0 ) {
         
         [[rdio player] playSource:[currentTrack rdioId]];
         [[rdio player] addObserver:self forKeyPath:@"position" options:NSKeyValueChangeReplacement context:nil];
