a68381e46584fefd77595f84294a82f0590e9b8a
Andrew Huynh <a5.t.huynh@gmail.com>
2012-04-26 12:11:57 -0700

    Prepping server for gunicorn/nginx stack

---
+++ backend/gunicornapp.py
@@ -0,0 +1,15 @@
+import sys, os
+
+# Only need to set this on Dreamhost
+PYTHON_INTERP 	 = None # "/home/athlabs/env/bin/python"
+PROJECT_NAME 	 = '/Users/Omicron/Documents/Current Work/Hacks/rockmyworld/backend'
+PROJECT_SETTINGS = 'server.settings.Dev'
+
+if PYTHON_INTERP and sys.executable != PYTHON_INTERP: 
+	os.execl( PYTHON_INTERP, PYTHON_INTERP, *sys.argv )
+
+os.chdir( PROJECT_NAME )
+sys.path.append( os.getcwd() )
+
+from server import create_app
+application = create_app( PROJECT_SETTINGS )
