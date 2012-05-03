52111a21f2a14cd240a99ef63891f0dcedf1d681
Peter <infinitexH@gmail.com>
2012-05-03 12:58:53 -0700

    Added mood list and basic moodController Class

---
+++ ios/play/Classes/Controllers/MoodController.h
@@ -0,0 +1,22 @@
+//
+//  MoodController.h
+//  rockmyworld
+//
+//  Created by Peter Zhao on 5/3/12.
+//  Copyright (c) 2012 athlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MoodController : NSObject
+
+{
+    //--// Mood Library
+    NSArray  *moodList;
+    
+}
+
+@property (nonatomic, retain) NSString *currentMood;
+
+
+@end
