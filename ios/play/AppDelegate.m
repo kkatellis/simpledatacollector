8530e66f8624c69ec77418b6412b178edf536da8
Kirsten <kirstenkoa@gmail.com>
2012-05-06 15:45:08 -0700

    changed info page's style

--- ios/play/AppDelegate.m
+++ ios/play/AppDelegate.m
@@ -18,7 +18,7 @@
 #define FEEDBACK_HIDE_INTERVAL      15
 // Prompt will show up after this many activity changes
 #define FEEDBACK_ACTIVITY_CHANGES   3
-// how long to wait before killing the app inthe background
+// how long to wait before killing the app in the background
 #define BACKGROUND_TIMER            60 * 1
 
 @implementation AppDelegate
@@ -161,26 +161,26 @@
      If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
      */
     
-  /*  waitingToKill = [NSTimer 
+    waitingToKill = [NSTimer 
                      scheduledTimerWithTimeInterval:BACKGROUND_TIMER
                         target: self 
                      selector: @selector(callExit) 
                             userInfo: nil 
-                            repeats: NO];  */
+                            repeats: NO];  
 
 }
 
 - (void) callExit
 {
     exit(0);
-}
+} 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
     /*
      Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
      */
-   /* if (waitingToKill != nil)
-        [waitingToKill invalidate];*/
+    if (waitingToKill != nil)
+        [waitingToKill invalidate];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
